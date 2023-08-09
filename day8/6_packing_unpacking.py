dd567da7aa9bbac1f8f35be742889e2c50ee64b2
rajnayabhari <nayabhariraj97@gmail.com>
2023-08-09 17:56:59 +0545

    modified

--- day8/6_packing_unpacking.py
+++ day8/6_packing_unpacking.py
@@ -7,18 +7,16 @@ print(vowels)  # ('a', 'e', 'i', 'o', 'u')
 a = 1, 2
 print(a)  # (1, 2)
 
-
 # unpacking
-a,b=1,2
-print(a) #1
-print(b) #2
+a, b = 1, 2
+print(a)  # 1
+print(b)  # 2
 
-a,b=(1,2)
-print(a) #1
-print(b) #2
-
+a, b = (1, 2)
+print(a)  # 1
+print(b)  # 2
 
 # Possible errors in packing and unpacking
 
-a,b,c=1,3 #Not enough value to unpack
-a,b=1,2,3 #too many values to unpack
+a, b, c = 1, 3  # Not enough value to unpack
+a, b = 1, 2, 3  # too many values to unpack
